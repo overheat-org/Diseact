d564c13a30dd8fe6c06fed4740606ad666d950f4
nexusprime-here <nicolasestevaolima104@gmail.com>
2024-07-17 10:34:38 -0300

    Removed Log

--- src/utils.js
+++ src/utils.js
@@ -13,22 +13,6 @@ export const SelectMenuVariant = {
     Mentionable: 4,
 };
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-const formatMessage = (type, args) => {
-	const prefix = chalk.bgBlue(`[${type}]`) + ' ' + chalk.blue('>>');
-	const formattedArgs = args.map(arg =>
-		typeof arg === 'object' ? util.inspect(arg, { showHidden: false, depth: null, colors: true }) : arg
-	);
-	return [prefix, ...formattedArgs];
-};
-
-export const log = (...args) => {
-	if (isDevelopment) {
-		console.log(...formatMessage('LOG', args));
-	}
-}
-
 export const defer = Promise.prototype.then.bind(Promise.resolve());
 
 export class CommandBuilder extends SlashCommandBuilder {
