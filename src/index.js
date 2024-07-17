d5bbe800775974157267396071719f3596c5f152
nexusprime-here <nicolasestevaolima104@gmail.com>
2024-07-17 10:34:29 -0300

    Added parser on export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { parseElement as parse } from './elements/parser';
 import { Fragment, createElement } from './jsx/create-element';
 import { useEffect, useState } from './component/hooks';
 import { render } from './render';
@@ -9,7 +10,8 @@ export {
 	render,
 	Fragment,
 	createElement,
-	SelectMenuVariant
+	SelectMenuVariant,
+	parse
 }
 
 export * as default from './index';
