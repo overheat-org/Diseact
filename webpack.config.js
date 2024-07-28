58208580c2fc97d17320a64055b45bdcc57f5c33
nexusprime-here <nicolasestevaolima104@gmail.com>
2024-07-28 17:23:33 -0300

    Fix weird js files generated, using babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
 const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 /**
  * @type {import('webpack').Configuration}
  */
 const config = {
     entry: {
-        "../lib/index": "./src/lib/index.js",
-        "../hooks/index": {
-            import: "./src/hooks/index.js"
-        },
-        "../jsx-runtime/index": {
-            import: "./src/jsx-runtime/index.js"
-        }
+        "lib/index": "./src/lib/index.js",
+        "hooks/index": "./src/hooks/index.js",
+        "jsx-runtime/index": "./src/jsx-runtime/index.js"
     },
     output: {
         filename: "[name].js",
-        path: path.resolve(__dirname, 'internal'),
+        path: __dirname,
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: "/node_modules/",
                 use: {
-                    loader: 'node-loader'
+                    loader: 'babel-loader',
                 },
             }
         ]
     },
     optimization: {
-        minimize: false,
+        minimize: false
     },
     plugins: [
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'src/hooks/*.d.ts', to: '../hooks/[name][ext]' },
-                { from: 'src/jsx-runtime/*.d.ts', to: '../jsx-runtime/[name][ext]' },
-                { from: 'src/lib/*.d.ts', to: '../lib/[name][ext]' },
+                { from: 'src/hooks/*.d.ts', to: 'hooks/[name][ext]' },
+                { from: 'src/jsx-runtime/*.d.ts', to: 'jsx-runtime/[name][ext]' },
+                { from: 'src/lib/*.d.ts', to: 'lib/[name][ext]' },
             ],
         }),
     ],
+    externals: {
+        'discord.js': 'commonjs discord.js'
+    },
     target: "node",
 }
 
