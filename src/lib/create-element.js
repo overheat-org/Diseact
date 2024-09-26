af7835adc902eb2dda0d615fbb06d6b968774496
Nexus <nicolasestevaolima104@gmail.com>
2024-09-26 18:15:58 -0300

    feat: now createElement can parse text elements

--- src/lib/create-element.js
+++ src/lib/create-element.js
@@ -1,7 +1,25 @@
 import { parseElement } from '../internal/parser';
 
+const EMPTY_ARRAY = Object.freeze([]);
+
 export function createElement(type, props, ...children) {
-	return parseElement({ type, props, children });
+	return parseElement({ 
+		type, 
+		props, 
+		children: children.map(c =>
+			typeof c == 'object' ? c : createTextElement(c)
+		) ?? EMPTY_ARRAY
+	});
+}
+
+function createTextElement(text) {
+	return {
+		type: "TEXT_ELEMENT",
+		props: {
+			value: text
+		},
+		children: EMPTY_ARRAY
+	}
 }
 
 export function Fragment(props) {
