0b84e09a3133dd90221d19c5aa158868c305c30b
Nexus <nicolasestevaolima104@gmail.com>
2024-12-04 23:32:37 -0300

    feat: including types now

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-BABEL_ENV=cjs babel src --out-dir cjs --copy-files
-BABEL_ENV=esm babel src --out-dir esm --copy-files
+export PATH=$PATH:./node_modules/.bin
+
+BABEL_ENV=cjs babel src --out-dir cjs
+BABEL_ENV=esm babel src --out-dir esm
+
+copyfiles -u 1 "src/**/*.d.ts" types
