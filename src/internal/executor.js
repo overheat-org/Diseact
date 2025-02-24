db9861695c8eaeeb26d4696dfec557501f9b4093
Nexus <nicolasestevaolima104@gmail.com>
2025-02-24 13:29:55 -0300

    chore: executor handles command map and jsx commands

--- src/internal/executor.js
+++ src/internal/executor.js
@@ -29,17 +29,19 @@ export class InteractionExecutor {
         (interaction.isChatInputCommand() ? this.commandMap : autocompleteMap)[path]?.(interaction);
     }
     
-    /** 
-     * @param {{ __map__: { [key: string]: Function } }[]} commands 
-     */
     putCommands(commands) {
-        let map = {}
+        if(Array.isArray(commands)) {
+            let map = {}
         
-        for(const command of commands) {
-
-            map = { ...map, ...command.__map__ }
+            for(const command of commands) {
+    
+                map = { ...map, ...command.__map__ }
+            }
+            
+            this.commandMap = map;
         }
-        
-        this.commandMap = map;
+        else {
+            this.commandMap = commands;
+        }
     }
 }
